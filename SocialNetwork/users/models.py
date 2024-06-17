ce8a84bb196fc758a14b19737f794f250296a88a
Truhin Andrey <truhin.andrey2812@gmail.com>
2024-06-17 22:00:05 +0300

    Начало создания БД профиля

--- SocialNetwork/users/models.py
+++ SocialNetwork/users/models.py
@@ -1,5 +1,14 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length=25, blank=True)
+    last_name = models.CharField(max_length=35, blank=True)
+    birth_date = models.DateField(blank=True)
+    main_photo = models.FilePathField(blank=True)
+    about_me = models.TextField(blank=True)
